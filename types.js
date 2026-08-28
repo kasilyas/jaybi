044aae7578e0b1170f868f1e7d103b7c6acf040f
Jaybi Dev <dev@jaybi.local>
2026-08-28 13:27:11 +0000

    chore: remove TS compilation artifacts, update .gitignore

--- types.js
+++
@@ -1,8 +0,0 @@
-export var StoreName;
-(function (StoreName) {
-    StoreName["MARJANE"] = "Marjane";
-    StoreName["CARREFOUR"] = "Carrefour";
-    StoreName["BIM"] = "BIM";
-    StoreName["ASWAK"] = "Aswak Assalam";
-})(StoreName || (StoreName = {}));
-//# sourceMappingURL=types.js.map
